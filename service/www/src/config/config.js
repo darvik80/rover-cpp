23b2e5fbd69b1ea86f66777adfc4b11487b2b56d
Ivan Kishchenko <darvik@crearts.xyz>
2019-08-18 11:13:51 +0700

    remove NODE_ENV

--- service/www/src/config/config.js
+++ service/www/src/config/config.js
@@ -1,7 +1,7 @@
 import JsonRpcClient from "react-jsonrpc-client";
 
 const config = {
-    api: new JsonRpcClient({endpoint: process.env.NODE_ENV.trim() === 'production' ? '/rpc' : 'http://localhost:3333'})
+    api: new JsonRpcClient({endpoint: '/rpc'})
 };
 
 export default config;
