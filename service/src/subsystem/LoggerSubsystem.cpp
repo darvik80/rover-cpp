b0a6f971f274a2c42bfcfca298e0fd8cd30b14d9
Ivan Kishchenko <darvik@crearts.xyz>
2021-01-24 11:09:20 +0700

    logs

--- service/src/subsystem/LoggerSubsystem.cpp
+++ service/src/subsystem/LoggerSubsystem.cpp
@@ -8,7 +8,14 @@
 #include <boost/log/expressions.hpp>
 #include <boost/log/utility/setup/file.hpp>
 #include <boost/log/utility/setup/console.hpp>
+
+#include <boost/log/expressions.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/utility/formatting_ostream.hpp>
+
+#include <boost/date_time/posix_time/posix_time_types.hpp>
+#include <boost/log/support/date_time.hpp>
+
 #include "subsystem/Application.h"
 #include "const.h"
 
@@ -20,6 +27,49 @@ const char *LoggerSubsystem::name() const {
     return "logger";
 }
 
+BOOST_LOG_ATTRIBUTE_KEYWORD(timestamp,
+                            "TimeStamp", log::attributes::local_clock::value_type)
+BOOST_LOG_ATTRIBUTE_KEYWORD(threadId,
+                            "ThreadID", log::attributes::current_thread_id::value_type)
+
+void consoleFormatter(log::record_view const &rec, log::formatting_ostream &strm) {
+
+    strm << rec[timestamp];
+
+    auto severity = rec[log::trivial::severity];
+    if (severity) {
+        // Set the color
+        switch (severity.get()) {
+            case log::trivial::severity_level::debug:
+                strm << "\033[37m";
+                break;
+
+            case log::trivial::severity_level::info:
+                strm << "\033[32m";
+                break;
+            case log::trivial::severity_level::warning:
+                strm << "\033[33m";
+                break;
+            case log::trivial::severity_level::error:
+            case log::trivial::severity_level::fatal:
+                strm << "\033[31m";
+                break;
+            default:
+                break;
+        }
+    }
+
+    strm << " <" << std::setw(7) << std::right << severity << "> ";
+    strm << "\033[36m[ " << rec[threadId] << " ]";
+    strm << "\033[0m : " << rec[log::expressions::smessage];
+}
+
+void fileFormatter(log::record_view const &rec, log::formatting_ostream &strm) {
+
+    strm << rec[timestamp] << " <" << std::setw(7) << std::right << rec[log::trivial::severity] << "> ";
+    strm << "[ " << rec[threadId] << " ] : " << rec[log::expressions::smessage];
+}
+
 void LoggerSubsystem::postConstruct(Application &app) {
     std::string filePath = app.getProperties()->getString(PROP_APPLICATION_LOG_DIR)
                            + app.getProperties()->getString(PROP_APPLICATION_NAME) + ".log";
@@ -29,16 +79,24 @@ void LoggerSubsystem::postConstruct(Application &app) {
     if (app.getProperties()->getBoolean(PROP_LOG_ENABLE_FILE, true)) {
         log::add_file_log(
                 log::keywords::file_name = filePath,
-                log::keywords::format = "[%TimeStamp%] [%ThreadID%] [%Severity%] %Message%",
+                log::keywords::rotation_size = 10 * 1024 * 1024,
+                log::keywords::time_based_rotation = log::sinks::file::rotation_at_time_point(0, 0, 0),
                 log::keywords::auto_flush = true
-        );
+        )->set_formatter(&fileFormatter);
+
+//        log::add_file_log(
+//                log::keywords::file_name = filePath,
+//                log::keywords::format = "[%TimeStamp%] [%ThreadID%] [%Severity%] %Message%",
+//                log::keywords::auto_flush = true
+//        );
     }
 
     if (app.getProperties()->getBoolean(PROP_LOG_ENABLE_CONSOLE, true)) {
-        log::add_console_log(
-                std::cout,
-                log::keywords::format = "[%TimeStamp%] [%ThreadID%] [%Severity%] %Message%"
-        );
+        log::add_console_log(std::cout)->set_formatter(&consoleFormatter);
+//        log::add_console_log(
+//                std::cout,
+//                log::keywords::format = "[%TimeStamp%] [%ThreadID%] [%Severity%] %Message%"
+//        );
     }
 
     auto logLevel = app.getProperties()->getString(PROP_LOG_LEVEL, "debug");
