7c28fa888b2a0cc97b4b9b6631cfe5638fecaab6
Ivan Kishchenko <darvik@crearts.xyz>
2021-07-15 22:45:00 +0700

    Barcode reader based on /linux/input

---
+++ service/src/BarcodeReaderService.cpp
@@ -0,0 +1,119 @@
+//
+// Created by Ivan Kishchenko on 15.07.2021.
+//
+
+#ifdef SUPPORT_BARCODE
+
+#include "BarcodeReaderService.h"
+
+char key[KEY_UNKNOWN] = { 0 };
+
+void initKey() {
+    key[KEY_MINUS] = '-';
+    key[KEY_0] = '0';
+    key[KEY_1] = '1';
+    key[KEY_2] = '2';
+    key[KEY_3] = '3';
+    key[KEY_4] = '4';
+    key[KEY_5] = '5';
+    key[KEY_6] = '6';
+    key[KEY_7] = '7';
+    key[KEY_8] = '8';
+    key[KEY_9] = '9';
+
+    key[KEY_Q] = 'Q';
+    key[KEY_W] = 'W';
+    key[KEY_E] = 'E';
+    key[KEY_R] = 'R';
+    key[KEY_T] = 'T';
+    key[KEY_Y] = 'Y';
+    key[KEY_U] = 'U';
+    key[KEY_I] = 'I';
+    key[KEY_O] = 'O';
+    key[KEY_P] = 'P';
+
+    key[KEY_A] = 'A';
+    key[KEY_S] = 'S';
+    key[KEY_D] = 'D';
+    key[KEY_F] = 'F';
+    key[KEY_G] = 'G';
+    key[KEY_H] = 'H';
+    key[KEY_J] = 'J';
+    key[KEY_K] = 'K';
+    key[KEY_L] = 'L';
+
+    key[KEY_Z] = 'Z';
+    key[KEY_X] = 'X';
+    key[KEY_C] = 'C';
+    key[KEY_V] = 'V';
+    key[KEY_B] = 'B';
+    key[KEY_N] = 'N';
+    key[KEY_M] = 'M';
+}
+
+
+void BarcodeReaderService::postConstruct(Registry &registry) {
+    initKey();
+    BaseService::postConstruct(registry);
+
+    int fd;
+    char name[256] = "Unknown";
+    const char *device = "/dev/input/by-id/usb-USB_Adapter_USB_Device-event-kbd";
+
+    //Open Device
+    if ((fd = open(device, O_RDONLY)) == -1) {
+        error("%s is not a valid device.n", device);
+        return;
+    }
+
+    //Print Device Name
+    ioctl(fd, EVIOCGNAME (sizeof(name)), name);
+    info("Reading From : {} ({})", device, name);
+
+    _stream = std::make_unique<boost::asio::posix::stream_descriptor>(
+            registry.getIoService()
+    );
+    _stream->assign(fd);
+    _stream->async_read_some(
+            boost::asio::buffer(&_events, sizeof(_events)),
+            [this](const boost::system::error_code error, std::size_t readable) {
+                onRead(error, readable);
+            }
+    );
+
+}
+
+void BarcodeReaderService::onRead(const boost::system::error_code &error, std::size_t readable) {
+    if (!error) {
+        std::size_t size = readable / sizeof(input_event);
+        for (int idx = 0; idx < size; idx++) {
+            if (_events[idx].value == 0) {
+                continue;
+            }
+            if (_events[idx].type == EV_KEY && _events[idx].value == 1) {
+                switch (_events[idx].code) {
+                    case KEY_LEFTSHIFT:
+                        continue;
+                    case KEY_ENTER:
+                        info("result: {}", _barcode);
+                        _barcode = "";
+                        continue;
+                    default:
+                        _barcode += key[_events[idx].code];
+                        break;
+                }
+            }
+        }
+
+        _stream->async_read_some(
+                boost::asio::buffer(&_events, sizeof(_events)),
+                [this](const boost::system::error_code error, std::size_t readable) {
+                    onRead(error, readable);
+                }
+        );
+    } else {
+        warning("read failed: {}", error.message());
+    }
+}
+
+#endif
---
+++ service/src/BarcodeReaderService.h
@@ -0,0 +1,29 @@
+//
+// Created by Ivan Kishchenko on 15.07.2021.
+//
+
+#ifndef ROVER_BARCODEREADERSERVICE_H
+#define ROVER_BARCODEREADERSERVICE_H
+
+#ifdef SUPPORT_BARCODE
+
+#include <BaseService.h>
+#include <linux/input.h>
+
+class BarcodeReaderService : public BaseService {
+    input_event _events[64];
+    std::unique_ptr<boost::asio::posix::stream_descriptor> _stream;
+
+    std::string _barcode;
+public:
+    const char *name() override {
+        return "barcode";
+    }
+
+    void postConstruct(Registry &registry) override;
+    void onRead(const boost::system::error_code &error, std::size_t readable);
+};
+
+#endif
+
+#endif //ROVER_BARCODEREADERSERVICE_H
